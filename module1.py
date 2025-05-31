4b12886383fe16fdbb663af014e58bc46eb49043
akhlad97 <your_email@example.com>
2025-05-31 22:34:23 +0530

    create module1.py file

---
+++ module1.py
@@ -0,0 +1,12 @@
+# mymodule.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
