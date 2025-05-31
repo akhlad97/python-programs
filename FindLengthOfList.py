294f169ceba9ad666af1e925c4264cf9a21b9ed4
akhlad97 <your_email@example.com>
2025-05-31 19:04:04 +0530

    Find the Length Of List

---
+++ FindLengthOfList.py
@@ -0,0 +1,13 @@
+# approach1
+lst = [1, 2, 3, 4, 5]
+count = 0
+# Using a for loop to count the elements in the list
+for i in lst:
+    count += 1
+    
+print("Length of the list is:", count)
+
+##approach2 using  built in function len() 
+
+length_of_list = len(lst)
+print("Length of the list using built-in function is:", length_of_list)
