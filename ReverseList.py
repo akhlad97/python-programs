ddd3098a8bac218849b1e939ebbb6e8431a3b14a
akhlad97 <your_email@example.com>
2025-06-05 15:33:23 +0530

    Reverse a List

---
+++ ReverseList.py
@@ -0,0 +1,11 @@
+mylist=[12,34,56,23,12,45,6,4,8]
+
+## using revers() method 
+mylist.reverse()
+print(mylist)
+
+# approach slicing technique
+
+mylist=[12,34,56,23,12,45,6,4,8]
+print("befor reverse ",mylist)
+print("after revres ",mylist[::-1])
