5273526779658c83d41b6baee3700d1589c8225a
akhlad97 <your_email@example.com>
2025-06-05 15:28:34 +0530

    Printing Numbers In Right Triangle Shape

---
+++ PrintingNumbersInRightTriangleShape.py
@@ -0,0 +1,19 @@
+# print the number in inceasing order
+n=int(input("enter the number of rows"))
+
+for i in range(1,n+1):
+    10
+    for j in range (1,i+1):
+        print(j,end=" ")
+    65
+    print()
+
+## print same number in rows 
+n=int(input("enter the number of rows"))
+
+for i in range(1,n+1):
+    10
+    for j in range (1,i+1):
+        print(i,end=" ")
+    
+    print()
